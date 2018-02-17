0d3661c8b66a6c0d390b93da69c0dffbd94c1d0d
Zheng <yuanzhengstl@gmail.com>
2018-02-16 17:33:56 -0800

    update migrations and post controllers and templates

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216064637) do
+ActiveRecord::Schema.define(version: 20180217010314) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
