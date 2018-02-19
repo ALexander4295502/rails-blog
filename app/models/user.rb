e91d88c665b33e0f92db392854ddb397569bfd96
Zheng <yuanzhengstl@gmail.com>
2018-02-19 13:15:08 -0800

    add edit comment routes and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_and_belongs_to_many :forums
   has_many :subscriptions
   has_many :magazines, through: :subscriptions
+  has_one :profile
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219201937) do
+ActiveRecord::Schema.define(version: 20180219203754) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 20180219201937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "body"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "twitter"
+    t.string   "github"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
